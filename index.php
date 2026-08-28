93729acc8c301b29ae1d303bd2fb8b9f508e2ef1
SiddiqueAsad <asad28052009@gmail.com>
2026-08-28 20:53:24 +0530

    Project Initialized

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Crud</title>
+</head>
+
+<body>
+    <h1>This is a Student Crud Management System</h1>
+</body>
+
+</html>
